f856e8adc10e06c10b241d96e527d3a0bfdca4c1
Konstantin-Mur <murashko.konst.andr@gmail.com>
2023-08-21 15:16:53 +0300

    Edit scripts, add scripts

--- Assets/Scripts/EventAgregator.cs
+++ Assets/Scripts/EventAgregator.cs
@@ -6,4 +6,6 @@ public class EventAgregator
     public static UnityEvent<float> editVolumeSound = new UnityEvent<float>();
     public static UnityEvent updateUI = new UnityEvent();
     public static UnityEvent playerLostHP = new UnityEvent();
+    public static UnityEvent playerDoShot = new UnityEvent();
+    public static UnityEvent<int> updateScore = new UnityEvent<int>();
 }
---
+++ Assets/Scripts/TestUI/BulletPackageTest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPackageTest : MonoBehaviour
+{
+    public int bulletCount = 5;
+    public int score = 100;
+    private Player _player;
+    private void Start()
+    {
+        _player = FindObjectOfType<Player>();
+    }
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _player.AddBullets(bulletCount);
+            _player.AddScore(score);
+            Destroy(this.gameObject);
+        }
+    }
+}
